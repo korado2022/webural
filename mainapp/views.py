32a65424c1cfd9378af19f036a390e2b429f051b
korado29 <korado_29@mail.ru>
2024-06-09 23:02:04 +0300

    new image for catalog

--- mainapp/views.py
+++ mainapp/views.py
@@ -79,6 +79,9 @@ class ServicePageView(TemplateView):
 class WorkingPageView(TemplateView):
     template_name = "mainapp/working.html"
 
+class PrivacyPageView(TemplateView):
+    template_name = "mainapp/privacy.html"
+
 class ContactPageView(TemplateView):
     template_name = "mainapp/contacts.html"
 
@@ -93,55 +96,54 @@ class ContactPageView(TemplateView):
 
 
 
-class PortfolioPageView(TemplateView):
-    template_name = "mainapp/portfolio.html"
+# class PortfolioPageView(TemplateView):
+#     template_name = "mainapp/portfolio.html"
+#
+#
+#
+# class DetailPageView(TemplateView):
+#     template_name = "mainapp/detail.html"
+#
+# class SensorStandsPageView(TemplateView):
+#     template_name = "mainapp/stendy-datchikov.html"
+#
+#
+# class CommissioningWorksPageView(TemplateView):
+#     template_name = "mainapp/puskonaladochnye-raboty.html"
+#
+# class ElectricalInstallationWorksPageView(TemplateView):
+#     template_name = "mainapp/elektromontazhnye-raboty.html"
+#
+# class DesignKipPageView(TemplateView):
+#     template_name = "mainapp/proektirovanie-kip.html"
+#
 
-
-
-class DetailPageView(TemplateView):
-    template_name = "mainapp/detail.html"
-
-class SensorStandsPageView(TemplateView):
-    template_name = "mainapp/stendy-datchikov.html"
-
-
-class CommissioningWorksPageView(TemplateView):
-    template_name = "mainapp/puskonaladochnye-raboty.html"
-
-class ElectricalInstallationWorksPageView(TemplateView):
-    template_name = "mainapp/elektromontazhnye-raboty.html"
-
-class DesignKipPageView(TemplateView):
-    template_name = "mainapp/proektirovanie-kip.html"
-
-class PrivacyPageView(TemplateView):
-    template_name = "mainapp/privacy.html"
-
-class KipaPageView(TemplateView):
-    template_name = "mainapp/kipa.html"
-
-class DetailexamPageView(TemplateView):
-    template_name = "mainapp/detail_exam.html"
-
-
-
-class CatalogDetailPageView(TemplateView):
-    template_name = "mainapp/catalog-detail.html"
-
-class CatalogCardBobyshkiPageView(TemplateView):
-    template_name = "mainapp/catalog-card-bobyshki.html"
-
-
-class BobyshkiTablePageView(TemplateView):
-    template_name = "mainapp/bobyshki-table.html"
-
-
-class ProductDetailPageView(TemplateView):
-    template_name = "mainapp/product-detail.html"
-
-
-class CatalogDescriptPageView(TemplateView):
-    template_name = "mainapp/catalog-descript.html"
+#
+# class KipaPageView(TemplateView):
+#     template_name = "mainapp/kipa.html"
+#
+# class DetailexamPageView(TemplateView):
+#     template_name = "mainapp/detail_exam.html"
+#
+#
+#
+# class CatalogDetailPageView(TemplateView):
+#     template_name = "mainapp/catalog-detail.html"
+#
+# class CatalogCardBobyshkiPageView(TemplateView):
+#     template_name = "mainapp/catalog-card-bobyshki.html"
+#
+#
+# class BobyshkiTablePageView(TemplateView):
+#     template_name = "mainapp/bobyshki-table.html"
+#
+#
+# class ProductDetailPageView(TemplateView):
+#     template_name = "mainapp/product-detail.html"
+#
+#
+# class CatalogDescriptPageView(TemplateView):
+#     template_name = "mainapp/catalog-descript.html"
 
 
 class SuccessPageView(TemplateView):
